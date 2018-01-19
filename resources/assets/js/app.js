9730a2c0c78c2d879cc2641ce687fa40e942e6f3
Trojan <ordrizzy@gmail.com>
2018-01-19 03:14:02 +0100

    Mix shit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import InstantSearch from 'vue-instantsearch';
+Vue.use(InstantSearch);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
